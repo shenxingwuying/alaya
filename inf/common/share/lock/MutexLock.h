f7fdeb03f110ea0c2a9fcc63d380738f9100fb0a
shenxingwuying <shenxingwuying@gmail.com>
2016-05-12 00:47:01 +0800

    Update MutexLock.h

--- inf/common/share/lock/MutexLock.h
+++ inf/common/share/lock/MutexLock.h
@@ -1,6 +1,23 @@
 #include <pthread.h>
 
 class MutexLock {
+public:
+  MutexLock() {
+    pthread_mutex_init(&mutex_, NULL);
+  }
+  ~/MutexLock() {
+    pthread_mutex_destory(&mutex_);
+  }
+  Lock() {
+    pthread_mutex_lock(&mutex_);
+  }
+  TryLock() {
+    pthread_mutex_trylock(&mutex_);
+  }
+  unLock() {
+    pthread_mutex_unlock(&mutex_);
+  }
 
-
+private:
+  pthread_mutex_t mutex_;
 }
