bbadb9961b68ebd2a7822243bb8a00f693db7bd4
shenxingwuying <shenxingwuying@gmail.com>
2016-05-12 00:53:21 +0800

    Update SpinLock.h

--- inf/common/share/lock/SpinLock.h
+++ inf/common/share/lock/SpinLock.h
@@ -1,9 +1,23 @@
 #include <pthread.h>
 
 class SpinLock {
-  public:
-    SpinLock();
+    public:
+        SpinLock() {
+            pthread_spin_init(&spin_, 0);
+        }
+        ~SpinLock() {
+            pthread_spin_destroy(&spin_);
+        }
+        void Lock() {
+            pthread_spin_lock(&spin_);
+        }
+        bool TryLock() {
+            pthread_spin_trylock(&spin_);
+        }
+        void Unlock() {
+            pthread_spin_unlock(&spin_);
+        }
 
-  private:
-    pthread_spin_t spin_;
+    private:
+        pthread_spin_t spin_;
 }
