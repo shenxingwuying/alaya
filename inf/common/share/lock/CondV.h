185d66677cf4ea5aecb04e38e91a0f3adda72827
shenxingwuying <shenxingwuying@gmail.com>
2016-05-12 01:08:47 +0800

    Update CondV.h

--- inf/common/share/lock/CondV.h
+++ inf/common/share/lock/CondV.h
@@ -1,7 +1,25 @@
 #include <pthread.h>
 
-
 class CondV {
-
-
+    public:
+        CondV() {
+            pthread_cond_init(&cond_, NULL);
+        }
+        ~CondV() {
+            pthread_cond_destory(&cond_);
+        }
+        void Wait(MutexLock& mutex) {
+            pthread_cond_wait(&cond_, &(mutex.lock_));
+        }
+        void Wait(MutexLock& mutex, int timeout) {
+            pthread_cond_timedwait(&cond_, &(mutex.lock_), timeout);
+        }
+        void Signal() {
+            pthread_cond_signal(&cond_);
+        }
+        void SignalAll() {
+            pthread_cond_broadcase(&cond_);
+        }
+    private:
+        pthread_cond_t cond_;
 }
