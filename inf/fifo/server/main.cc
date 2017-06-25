e51b60299e4dbdeaf3d992188580fd63864a9bd7
duyuqi <DUYUQI@kingsoft.com>
2017-06-26 00:05:59 +0800

    add a readme

--- inf/fifo/server/main.cc
+++ inf/fifo/server/main.cc
@@ -17,10 +17,21 @@
 
 int main(int argc, char* argv[]) {
     PosixWritableFile file("test.txt");
-    file.Append("abc");
-    file.Append("123");
-    file.Flush();
-    file.Close();
+    //file.Append("abc\n");
+    //file.Append("123\n");
+    int i = 0;
+    std::string str("");
+    char buff[1024] = {0};
+    while (true) {
+        snprintf(buff, 1024, "my_test_key_%d\n", i);
+        file.Append(buff);
+        memset(buff, 0, 1024);
+        i++;
+    }
+//    file.Flush();
+    file.Append("abc_end\n");
+    file.Append("123_end\n");
+//    file.Close();
     return 0;
 }
 
